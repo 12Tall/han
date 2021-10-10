2750e18ad9701277486bbbdc1fdda0bedc2e7b87
12tall <fb.ouyang@outlook.com>
2021-10-10 21:57:16 +0800

    test

--- test.js
+++ test.js
@@ -1,10 +1,2 @@
 var addon = require('.');
-addon.test_threadsafe_function((err, ...values) => {
-  var i = 0;
-  while (i++ < 10) {
-    console.log(">> 子线程 <<")
-  }
-});
-setTimeout(() => {
-  console.log(" > 主线程 <")
-}, 10000);
+addon.hello("12tall");
