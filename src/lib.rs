471cf1c7f50b7bbe312f195596ddb99bc7483827
12tall <fb.ouyang@outlook.com>
2021-10-10 17:53:40 +0800

    添加CI

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,8 @@ extern crate napi_derive;
 // use napi::bindgen_prelude::*;
 use napi::{
     threadsafe_function::{ThreadSafeCallContext, ThreadsafeFunctionCallMode},
-    CallContext, Env, JsFunction, JsNumber, JsObject, JsUndefined, Result as JsResult,
+    CallContext, Env, JsFunction, JsNumber, JsObject, JsString, JsStringUtf8, JsUndefined,
+    Result as JsResult,
 };
 use std::{convert::TryInto, thread, time::Duration};
 
@@ -23,45 +24,20 @@ fn fibonacci_native(n: i64) -> i64 {
 }
 
 #[js_function(1)]
-pub fn test_threadsafe_function(ctx: CallContext) -> JsResult<JsUndefined> {
-    let func = ctx.get::<JsFunction>(0)?;
+pub fn hello(ctx: CallContext) -> JsResult<JsString> {
+    let input = ctx.get::<JsString>(0)?;
+    let input = input.into_utf16()?.as_str()?;
 
-    let tsfn =
-        ctx.env
-            .create_threadsafe_function(&func, 0, |ctx: ThreadSafeCallContext<Vec<u32>>| {
-                ctx.value
-                    .iter()
-                    .map(|v| ctx.env.create_uint32(*v))
-                    .collect::<JsResult<Vec<JsNumber>>>()
-            })?;
+    let mut output = String::from("hello ");
+    output.push_str(&input);
 
-    let tsfn_cloned = tsfn.clone();
-
-    // thread::spawn(move || {
-    //     let output: Vec<u32> = vec![0, 1, 2, 3];
-    //     // It's okay to call a threadsafe function multiple times.
-    //     for i in 0..100 {
-    //         thread::sleep(Duration::from_millis(400));
-    //         tsfn.call(Ok(output.clone()), ThreadsafeFunctionCallMode::Blocking);
-    //     }
-    // });
-
-    thread::spawn(move || {
-        for i in 0..100 {
-            let output: Vec<u32> = vec![3, 2, 1, 0];
-            thread::sleep(Duration::from_millis(200));
-            // It's okay to call a threadsafe function multiple times.
-            tsfn_cloned.call(Ok(output.clone()), ThreadsafeFunctionCallMode::NonBlocking);
-        }
-    });
-
-    ctx.env.get_undefined()
+    ctx.env.create_string(&output)
 }
 
 #[module_exports]
 fn init(mut exports: JsObject, env: Env) -> JsResult<()> {
     exports.create_named_method("fibonacci", fibonacci)?;
-    exports.create_named_method("test_threadsafe_function", test_threadsafe_function)?;
+    exports.create_named_method("hello", hello)?;
     exports.set_named_property("DEFAULT_VALUE", env.create_int64(100)?)?;
     Ok(())
 }
